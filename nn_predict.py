2ed35f4d275d23b90d713c3c87e849a696240b78
Jason <t113318140@ntut.org.tw>
2025-06-10 14:41:27 +0800

    final exam

--- nn_predict.py
+++ nn_predict.py
@@ -3,13 +3,22 @@ import json
 
 # === Activation functions ===
 def relu(x):
-    # TODO: Implement the Rectified Linear Unit
-     return np.maximum(0, x)
+    return np.maximum(0, x)
 
 def softmax(x):
-    # TODO: Implement the SoftMax function
-    e_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
-    return e_x / np.sum(e_x, axis=-1, keepdims=True)
+    x = np.asarray(x, dtype=np.float64)
+    if x.ndim == 1:
+        x = x.reshape(1, -1)
+    if x.size == 0:
+        return x
+    x_max = np.max(x, axis=-1, keepdims=True)
+    e_x = np.exp(x - x_max)
+    sum_e_x = np.sum(e_x, axis=-1, keepdims=True)
+    sum_e_x = np.where(sum_e_x == 0, 1.0, sum_e_x)
+    result = e_x / sum_e_x
+    if result.shape[0] == 1:
+        result = result.flatten()
+    return result
 
 # === Flatten ===
 def flatten(x):
@@ -17,9 +26,9 @@ def flatten(x):
 
 # === Dense layer ===
 def dense(x, W, b):
-   return x @ W + b
+    return x @ W + b
+
 # Infer TensorFlow h5 model using numpy
-# Support only Dense, Flatten, relu, softmax now
 def nn_forward_h5(model_arch, weights, data):
     x = data
     for layer in model_arch:
@@ -27,7 +36,6 @@ def nn_forward_h5(model_arch, weights, data):
         ltype = layer['type']
         cfg = layer['config']
         wnames = layer['weights']
-
         if ltype == "Flatten":
             x = flatten(x)
         elif ltype == "Dense":
@@ -38,10 +46,7 @@ def nn_forward_h5(model_arch, weights, data):
                 x = relu(x)
             elif cfg.get("activation") == "softmax":
                 x = softmax(x)
-
     return x
 
-
-# You are free to replace nn_forward_h5() with your own implementation 
 def nn_inference(model_arch, weights, data):
     return nn_forward_h5(model_arch, weights, data)
